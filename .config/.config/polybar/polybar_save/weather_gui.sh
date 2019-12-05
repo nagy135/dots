859326e0288e0f0ebdba0b4aa85b146065ebf80e
nagy135 <viktor.nagy@gmail.com>
2019-12-05 23:29:08 +0100

    cleanup

--- .config/.config/polybar/polybar_save/weather_gui.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-xdg-open 'https://duckduckgo.com/?q=weather'
--- Code/scripts/scripts/i3status.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-i3status | while :
-do
-    read line
-    bright=`xbacklight -get`
-    echo "$bright | $line" || exit 1
-done
