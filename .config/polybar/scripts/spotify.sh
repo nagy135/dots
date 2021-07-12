b34ba0a7f2ae0c580f0ec85321238b334ecf0957
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-07-12 15:19:28 +0200

    feat(polybar): spotify, standalone script (playerctl with correct error handling)

---
+++ .config/polybar/scripts/spotify.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+data=$(playerctl metadata 2> /dev/null)
+[ $? -ne 0 ] \
+    && echo " " \
+    && exit 0
+
+echo "$data" | grep "xesam:title\|xesam:artist" | sort | tail -2 | sed 's/[a-z]* xesam:title\s*/: / ; s/[a-z]* xesam:artist\s*//' | tr -d '\n' | sed 's/^\(.\{45\}\).*/\1.../'
