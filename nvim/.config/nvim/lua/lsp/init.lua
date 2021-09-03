7bacda27a0cdefd7d44b48db0d4f2286b274d42e
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-09-03 18:50:42 +0200

    feat(nvim): lsp lua-language-server

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -1,3 +1,4 @@
 require("lsp.intelephense")
 require("lsp.tsserver")
 require("lsp.cssls")
+require("lsp.lua-language-server")
---
+++ nvim/.config/nvim/lua/lsp/lua-language-server.lua
@@ -0,0 +1,34 @@
+-- sudo pacman -S lua-language-server
+
+local sumneko_root_path = "/usr/share/lua-language-server"
+local sumneko_binary = "/usr/bin/lua-language-server"
+
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+require'lspconfig'.sumneko_lua.setup {
+  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = runtime_path,
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
