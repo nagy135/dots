7cece7011e8e99acd436f372ef4276c564370747
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-11-03 15:22:28 +0100

    fix(vimrc): broken CR bind

--- nvim/.config/nvim/lua/my_nvim-cmp.lua
+++ nvim/.config/nvim/lua/my_nvim-cmp.lua
@@ -17,7 +17,6 @@ cmp.setup({
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
         ["<c-y>"] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Insert,
             select = true,
