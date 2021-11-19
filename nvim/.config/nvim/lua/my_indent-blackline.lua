94d6213c83e957e2603f7f2852bf9abffe87296e
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-11-19 11:20:46 +0100

    feat(nvim): intent-blackline - alternative config with "indent guides"

--- nvim/.config/nvim/lua/my_indent-blackline.lua
+++ nvim/.config/nvim/lua/my_indent-blackline.lua
@@ -2,3 +2,20 @@ require("indent_blankline").setup {
     char = "|",
     buftype_exclude = {"terminal"}
 }
+
+-- INDENT GUIDES ALTERNATIVE
+-- vim.cmd [[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]]
+--
+-- require("indent_blankline").setup {
+--     char = "",
+--     char_highlight_list = {
+--         "IndentBlanklineIndent1",
+--         "IndentBlanklineIndent2",
+--     },
+--     space_char_highlight_list = {
+--         "IndentBlanklineIndent1",
+--         "IndentBlanklineIndent2",
+--     },
+--     show_trailing_blankline_indent = false,
+-- }
