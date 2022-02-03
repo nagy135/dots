934f40ccebd17582807e34d25de6c265f71cec3f
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-02-03 19:36:48 +0100

    feat(nix): envs

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -21,6 +21,14 @@
     enable = true;
   };
 
+  environment.sessionVariables = rec {
+      XDG_CACHE_HOME  = "\${HOME}/.cache";
+      XDG_CONFIG_HOME = "\${HOME}/.config";
+      XDG_BIN_HOME    = "\${HOME}/.local/bin";
+      XDG_DATA_HOME   = "\${HOME}/.local/share";
+  };
+
+
   # Set your time zone.
   time.timeZone = "Europe/Bratislava";
 
