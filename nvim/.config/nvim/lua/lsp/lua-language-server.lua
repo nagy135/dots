0d5a884fe3c71a12054cc6bd32e9e9d46a92f88e
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-02-05 16:35:41 +0100

    fix(nvim): lua lsp path back to default

--- nvim/.config/nvim/lua/lsp/lua-language-server.lua
+++ nvim/.config/nvim/lua/lsp/lua-language-server.lua
@@ -1,14 +1,14 @@
 -- sudo pacman -S lua-language-server
 
-local sumneko_root_path = "/usr/share/lua-language-server"
-local sumneko_binary = "/usr/bin/lua-language-server"
+-- local sumneko_root_path = "/usr/share/lua-language-server"
+-- local sumneko_binary = "/usr/bin/lua-language-server"
 
 local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
 require'lspconfig'.sumneko_lua.setup {
-  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+  -- cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
   settings = {
     Lua = {
       runtime = {
