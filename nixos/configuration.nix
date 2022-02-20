cab1a2a873dec4bdfcab3c844f690bf800b374b3
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-02-20 09:59:15 +0100

    fix(nixos): fontconfig header alignment

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -60,7 +60,7 @@
     enableDefaultFonts = true;
     fontconfig = {
       localConf = ''
-              <?xml version='1.0'?>
+        <?xml version='1.0'?>
         <!DOCTYPE fontconfig SYSTEM 'fonts.dtd'>
         <fontconfig>
          <alias>
