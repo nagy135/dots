8d66e0e5b334dffde80cf28ac155f5c46e81b531
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-03-16 18:27:38 +0100

    feat(nvim): luasnip first snippets

--- nvim/.config/nvim/lua/my_luasnip.lua
+++ nvim/.config/nvim/lua/my_luasnip.lua
@@ -4,8 +4,7 @@ if (status) then return lib end
     return nil
 end
 
-local luasnip = prequire('luasnip')
-local cmp = prequire("cmp")
+local ls = prequire('luasnip')
 
 local t = function(str)
     return vim.api.nvim_replace_termcodes(str, true, true, true)
@@ -21,21 +20,21 @@ local check_back_space = function()
 end
 
 _G.tab_complete = function()
-    if cmp and cmp.visible() then
-        cmp.select_next_item()
-    elseif luasnip and luasnip.expand_or_jumpable() then
+    if vim.fn.pumvisible() == 1 then
+        return t "<C-n>"
+    elseif ls and ls.expand_or_jumpable() then
         return t("<Plug>luasnip-expand-or-jump")
     elseif check_back_space() then
         return t "<Tab>"
     else
-        cmp.complete()
+        return vim.fn['compe#complete']()
     end
     return ""
 end
 _G.s_tab_complete = function()
-    if cmp and cmp.visible() then
-        cmp.select_prev_item()
-    elseif luasnip and luasnip.jumpable(-1) then
+    if vim.fn.pumvisible() == 1 then
+        return t "<C-p>"
+    elseif ls and ls.jumpable(-1) then
         return t("<Plug>luasnip-jump-prev")
     else
         return t "<S-Tab>"
@@ -43,147 +42,37 @@ _G.s_tab_complete = function()
     return ""
 end
 
+vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("i", "<C-k>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<C-k>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("i", "<C-j>", "v:lua.s_tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<C-j>", "v:lua.s_tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("i", "<C-E>", "<Plug>luasnip-next-choice", {})
 vim.api.nvim_set_keymap("s", "<C-E>", "<Plug>luasnip-next-choice", {})
 
--- SNIPPETS configuration
-
-
-local ls = require("luasnip")
--- some shorthands...
-local s = ls.snippet
+local fmt = require('luasnip.extras.fmt').fmt
+local s = ls.s
 local i = ls.insert_node
--- local f = ls.function_node
--- local t = ls.text_node
--- local r = ls.restore_node
--- local l = require("luasnip.extras").lambda
--- local rep = require("luasnip.extras").rep
--- local p = require("luasnip.extras").partial
--- local m = require("luasnip.extras").match
--- local n = require("luasnip.extras").nonempty
--- local dl = require("luasnip.extras").dynamic_lambda
--- local fmt = require("luasnip.extras.fmt").fmt
--- local fmta = require("luasnip.extras.fmt").fmta
--- local types = require("luasnip.util.types")
--- local conds = require("luasnip.extras.expand_conditions")
---
--- If you're reading this file for the first time, best skip to around line 190
--- where the actual snippet-definitions start.
+local rep = require('luasnip.extras').rep
 
--- WAITING FOR VIM.KEYMAP TO MAP LUA FUNCTION TO BIND DIRECTLY!!!!!!
--- <c-k> is my expansion key
--- this will expand the current item or jump to the next item within the snippet.
--- vim.keymap.set({ "i", "s" }, "<c-k>", function()
---   if ls.expand_or_jumpable() then
---     ls.expand_or_jump()
---   end
--- end, { silent = true })
---
---     -- <c-j> is my jump backwards key.
--- -- this always moves to the previous item within the snippet
--- vim.keymap.set({ "i", "s" }, "<c-j>", function()
---   if ls.jumpable(-1) then
---     ls.jump(-1)
---   end
--- end, { silent = true })
---
--- -- <c-l> is selecting within a list of options.
--- -- This is useful for choice nodes (introduced in the forthcoming episode 2)
--- vim.keymap.set("i", "<c-l>", function()
---   if ls.choice_active() then
---     ls.change_choice(1)
---   end
--- end)
-
-
--- Every unspecified option will be set to the default.
-ls.config.set_config({
-	history = true,
-	-- Update more often, :h events for more info.
-	updateevents = "TextChanged,TextChangedI",
-	-- ext_opts = {
-	-- 	[types.choiceNode] = {
-	-- 		active = {
-	-- 			virt_text = { { "choiceNode", "Comment" } },
-	-- 		},
-	-- 	},
-	-- },
-	-- treesitter-hl has 100, use something higher (default is 200).
-	ext_base_prio = 300,
-	-- minimal increase in priority.
-	ext_prio_increase = 1,
-	enable_autosnippets = true,
-})
-
--- args is a table, where 1 is the text in Placeholder 1, 2 the text in
--- placeholder 2,...
--- local function copy(args)
--- 	return args[1]
--- end
-
--- -- 'recursive' dynamic snippet. Expands to some text followed by itself.
--- local rec_ls
--- rec_ls = function()
--- 	return sn(
--- 		nil,
--- 		c(1, {
--- 			-- Order is important, sn(...) first would cause infinite loop of expansion.
--- 			t(""),
--- 			sn(nil, { t({ "", "\t\\item " }), i(1), d(2, rec_ls, {}) }),
--- 		})
--- 	)
--- end
-
--- in a lua file: search lua-, then c-, then all-snippets.
-ls.filetype_extend("lua", { "c" })
--- in a cpp file: search c-snippets, then all-snippets only (no cpp-snippets!!).
-ls.filetype_set("cpp", { "c" })
-
---[[
--- Beside defining your own snippets you can also load snippets from "vscode-like" packages
--- that expose snippets in json files, for example <https://github.com/rafamadriz/friendly-snippets>.
--- Mind that this will extend  `ls.snippets` so you need to do it after your own snippets or you
--- will need to extend the table yourself instead of setting a new one.
-]]
-
-require("luasnip.loaders.from_vscode").load({ include = { "python" } }) -- Load only python snippets
--- The directories will have to be structured like eg. <https://github.com/rafamadriz/friendly-snippets> (include
--- a similar `package.json`)
-require("luasnip.loaders.from_vscode").load({ paths = { "./my-snippets" } }) -- Load snippets from my-snippets folder
-
--- You can also use lazy loading so you only get in memory snippets of languages you use
-require("luasnip.loaders.from_vscode").lazy_load() -- You can pass { paths = "./my-snippets/"} as well
-
--- You can also use snippets in snipmate format, for example <https://github.com/honza/vim-snippets>.
--- The usage is similar to vscode.
-
--- One peculiarity of honza/vim-snippets is that the file with the global snippets is _.snippets, so global snippets
--- are stored in `ls.snippets._`.
--- We need to tell luasnip that "_" contains global snippets:
-ls.filetype_extend("all", { "_" })
-
-require("luasnip.loaders.from_snipmate").load({ include = { "c" } }) -- Load only python snippets
-
-require("luasnip.loaders.from_snipmate").load({ path = { "./my-snippets" } }) -- Load snippets from my-snippets folder
--- If path is not specified, luasnip will look for the `snippets` directory in rtp (for custom-snippet probably
--- `~/.config/nvim/snippets`).
-
-require("luasnip.loaders.from_snipmate").lazy_load() -- Lazy loading
-
-ls.snippets = {
-    -- javascript = js_ts,
-    -- typescript = js_ts,
+local ts_js = {
+    s('func', fmt("const {} = ({}) => {{\n  {}\n}};", {
+        i(1),
+        i(2),
+        i(0),
+    })),
+    s('log', fmt("console.log('{}',{})", {
+        rep(1),
+        i(1)
+    })),
+    -- ls.parser.parse_snippet('func', "const $1 = ($2) => {\n  $0\n};")
+    -- ls.parser.parse_snippet('log', "console.log($1)")
 }
 
--- autotriggered snippets have to be defined in a separate table, luasnip.autosnippets.
--- ls.autosnippets = {
---     all = {
---         s("autotrigger", {
---             t("autosnippet"),
---         }),
---     },
--- }
+ls.snippets = {
+    javascript = ts_js,
+    typescript = ts_js,
+}
