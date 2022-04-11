c1b8be9e4d03e17d2d1aacdff42e107c5c83ebca
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-04-11 15:36:18 +0200

    feat(nvim): sumneko_lua opts using lua-dev

--- nvim/.config/nvim/lua/lsp/my_nvim-lsp-installer.lua
+++ nvim/.config/nvim/lua/lsp/my_nvim-lsp-installer.lua
@@ -4,14 +4,9 @@ local lsp_installer = require("nvim-lsp-installer")
 -- or if the server is already installed).
 lsp_installer.on_server_ready(function(server)
     local opts = {}
+    if server.name == "sumneko_lua" then
+        opts = require("lua-dev").setup {}
+    end
 
-    -- (optional) Customize the options passed to the server
-    -- if server.name == "tsserver" then
-    --     opts.root_dir = function() ... end
-    -- end
-
-    -- This setup() function will take the provided server configuration and decorate it with the necessary properties
-    -- before passing it onwards to lspconfig.
-    -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
     server:setup(opts)
 end)
