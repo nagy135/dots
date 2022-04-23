3676e77a4c1e236e7e2ede5fe2b7af0029ad4734
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-04-23 21:34:41 +0200

    feat(nixos): commented "tutorial" to override packages

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -6,31 +6,48 @@ let
   mpv_history = import ./mpv_history.nix;
   torque = import ./torque.nix;
 
-  overridenFuzzel = unstable.fuzzel.overrideAttrs (oldAttrs: rec {
-    version = "1.7.x";
+  # import package from specific revision of nixpkgs
+  #
+  # myNixpkgs = import (builtins.fetchGit {
+  #       name = "nixos-unstable-2022-04-18";
+  #       url = https://github.com/nixos/nixpkgs/;
+  #       rev = "33cf95ef36d9e2e7aec511297de9a845d6b729fe";
+  #   }) {};
 
-    src = pkgs.fetchFromGitea {
-      domain = "codeberg.org";
-      owner = "dnkl";
-      repo = "fuzzel";
-      rev = "8e09393463a00fd233b8d3b2ac316c2cfea6c791";
-      sha256 = "1ywknv374yca0w862njlyn3bzcvigw3fskpa34ak8hycm05s2jfm";
-    };
 
-  });
 
-  overridenNeovim = unstable.neovim.overrideAttrs (oldAttrs: rec {
-    version = "0.6.1";
-    vimAlias = true;
+  # nixpkgs.overlays = [
+  #   (self: super:
+  #     {
+  #       yarn = super.yarn.overrideAttrs (old: {
+  #         nodejs = pkgs.nodejs-17_x;
+  #       });
+  #     })
+  # ];
+  #
+  #
+  # overridenFuzzel = unstable.fuzzel.overrideAttrs (oldAttrs: rec {
+  #   version = "1.7.x";
 
-    src = pkgs.fetchFromGitHub {
-      owner = "neovim";
-      repo = "neovim";
-      rev = "4e5e0076cb643b52d762f226b27f22c8fb837567";
-      sha256 = "1bzvg0a8sj9y8js68x0djb9njz3rhmy25cz3f39b9db7llggc1pk";
-    };
+  #   src = pkgs.fetchFromGitea {
+  #     domain = "codeberg.org";
+  #     owner = "dnkl";
+  #     repo = "fuzzel";
+  #     rev = "8e09393463a00fd233b8d3b2ac316c2cfea6c791";
+  #     sha256 = "1ywknv374yca0w862njlyn3bzcvigw3fskpa34ak8hycm05s2jfm";
+  #   };
 
-  });
+  # });
+
+  # overridenLazygit = unstable.lazygit.overrideAttrs (oldAttrs: rec {
+  #   src = pkgs.fetchFromGitHub {
+  #     owner = "jesseduffield";
+  #     repo = "lazygit";
+  #     rev = "09bc6f2aef2bc0c0d76d0b12498ec8976c822ff2";
+  #     sha256 = "sha256-LJXXS+nfQvdesRC3+WU1MVRm3nhrUcTTOF3jKzCHLpE=";
+  #   };
+  #
+  # });
 
   personalPackages = with pkgs; [
     subtube
@@ -56,7 +73,6 @@ let
     wget
     google-chrome
     git
-    python3
     qutebrowser
     gopls
     mpv
