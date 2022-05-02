dd20dd89beb31e7de4e674a42ce07ec0f92c0050
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-05-02 06:53:47 +0200

    feat(nvim): copy_with_difference

--- nvim/.config/nvim/lua/scripts.lua
+++ nvim/.config/nvim/lua/scripts.lua
@@ -7,6 +7,55 @@ local signature_lines = {
     "*/"
 }
 
+local function get_visual_selection()
+    local s_start = vim.fn.getpos("'<")
+    local s_end = vim.fn.getpos("'>")
+    local n_lines = math.abs(s_end[2] - s_start[2]) + 1
+    local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
+    lines[1] = string.sub(lines[1], s_start[3], -1)
+    if n_lines == 1 then
+        lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+    else
+        lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
+    end
+    return table.concat(lines, '\n')
+end
+
+_G.record_memory = {
+    selected = "";
+    selected_len = 0;
+    current_line = "";
+    col = 0;
+    row = 0;
+}
+
+_G.copy_with_difference = function(record)
+
+    if record == true then
+        record_memory.selected = get_visual_selection()
+        if record_memory.selected == "" then
+            print("select something!")
+            return
+        end
+        record_memory.selected_len = record_memory.selected:len()
+        record_memory.current_line = vim.api.nvim_get_current_line()
+        local cursor = vim.api.nvim_win_get_cursor(0)
+        record_memory.row = cursor[1]
+        record_memory.col = cursor[2]
+    elseif record_memory.selected == "" then
+        print("first select using copy_with_difference(true)")
+    else
+        local value = vim.fn.input("Update to: ")
+        if value == nil or value == '' then
+            return
+        end
+        local updated_line = string.sub(record_memory.current_line, 1, record_memory.col)
+            .. value
+            .. string.sub(record_memory.current_line, record_memory.col + record_memory.selected_len + 1, -1)
+
+        api.nvim_buf_set_lines(0, record_memory.row, record_memory.row, false, { updated_line })
+    end
+end
 _G.signature = function()
     local status, Input = pcall(require, "nui.input")
     if (status) then
