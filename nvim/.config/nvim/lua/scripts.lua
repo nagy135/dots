aa983bea1027c8ae8439ec7e035fa6be2eb3a28c
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-05-02 06:54:22 +0200

    feat(nvim): tabulize

--- nvim/.config/nvim/lua/scripts.lua
+++ nvim/.config/nvim/lua/scripts.lua
@@ -7,6 +7,17 @@ local signature_lines = {
     "*/"
 }
 
+local function split_by_sep(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+    local t = {}
+    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+        table.insert(t, str)
+    end
+    return t
+end
+
 local function get_visual_selection()
     local s_start = vim.fn.getpos("'<")
     local s_end = vim.fn.getpos("'>")
@@ -56,6 +67,99 @@ _G.copy_with_difference = function(record)
         api.nvim_buf_set_lines(0, record_memory.row, record_memory.row, false, { updated_line })
     end
 end
+
+--   +----------+                                               $
+--   | cryptoBE +----- POST /v1/bla --+     +--------+          $
+--   +-----+----+                     +-----+ nft BE |          $
+--         |     +-------------+            +--------+          $
+--         +-----+ payment api |                                $
+--               +-------------+                                $
+--                                                              $
+--                                                              $
+--                                                              $
+--                                                              $
+_G.tabulize = function()
+    local tl = '+'
+    local tr = '+'
+    local bl = '+'
+    local br = '+'
+
+    local v = '|'
+    local h = '-'
+
+    local s_start = vim.fn.getpos("'<")
+    local s_end = vim.fn.getpos("'>")
+    local row_start, col_start, row_end, col_end = s_start[2], s_start[3], s_end[2], s_end[3]
+
+    local width = col_end - col_start
+    local height = row_end - row_start
+
+    if row_start == row_end then
+        -- -
+        local previous_lines = api.nvim_buf_get_lines(0, row_start - 1, row_end, false)
+        local lines = {
+            string.sub(previous_lines[1], 1, col_start - 1)
+                .. "+"
+                .. string.rep("-", width - 1)
+                .. "+"
+                .. string.sub(previous_lines[1], col_end + 1) }
+        api.nvim_buf_set_lines(0, row_start - 1, row_start, false, lines)
+        return
+    elseif col_start == col_end then
+        -- |
+        local previous_lines = api.nvim_buf_get_lines(0, row_start - 1, row_end, false)
+        local lines = {
+            string.sub(previous_lines[1], 1, col_start - 1)
+                .. "+"
+                .. string.sub(previous_lines[1], col_end + 1) }
+        for i = 1, height - 1 do
+            table.insert(lines, string.sub(previous_lines[i + 1], 1, col_start - 1)
+            .. "|"
+            .. string.sub(previous_lines[i + 1], col_end + 1))
+        end
+        table.insert(lines, string.sub(previous_lines[height + 1], 1, col_start - 1)
+        .. "+"
+        .. string.sub(previous_lines[height + 1], col_end + 1))
+
+        api.nvim_buf_set_lines(0, row_start - 1, row_end, false, lines)
+        return
+    end
+
+    if row_start > row_end then
+        local tmp = row_end
+        row_end = row_start
+        row_start = tmp
+    end
+
+    if col_start > col_end then
+        local tmp = col_end
+        col_end = col_start
+        col_start = tmp
+    end
+
+
+    local previous_lines = api.nvim_buf_get_lines(0, row_start - 1, row_end, false)
+
+    local lines = { string.sub(previous_lines[1], 1, col_start - 1) .. tl .. string.rep(h, width - 1) .. tr .. string.sub(previous_lines[1], col_end + 1) }
+    for i = 1, height - 1 do
+        table.insert(lines,
+            string.sub(previous_lines[i + 1], 1, col_start - 1) ..
+            v ..
+            string.sub(previous_lines[i + 1], col_start + 1, col_end - 1) ..
+            v ..
+            string.sub(previous_lines[i + 1], col_end + 1)
+        )
+    end
+    table.insert(lines, string.sub(previous_lines[height + 1], 1, col_start - 1) ..
+    bl ..
+    string.rep(h, width - 1) ..
+    br ..
+    string.sub(previous_lines[height + 1], col_end + 1)
+    )
+
+    api.nvim_buf_set_lines(0, row_start - 1, row_end, false, lines)
+end
+
 _G.signature = function()
     local status, Input = pcall(require, "nui.input")
     if (status) then
