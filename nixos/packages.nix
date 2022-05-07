0c68e28b6ab48b87a74d1b2bf1bf86a0188dd89e
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-05-07 06:22:22 +0200

    feat(nix): steam

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -172,6 +172,14 @@ in
       wl-clipboard
     ];
   };
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
+
   services.transmission = {
     enable = true;
 
