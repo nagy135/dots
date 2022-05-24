3fa600b3d4b3f79e3bcc394f51b88675b4fd0a44
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-05-24 13:30:12 +0200

    fix(nvim): clashing bind <leader>tb

--- nvim/.config/nvim/lua/my_gitsigns.lua
+++ nvim/.config/nvim/lua/my_gitsigns.lua
@@ -68,7 +68,7 @@ require('gitsigns').setup {
         map('n', '<leader>hR', gs.reset_buffer)
         map('n', '<leader>hp', gs.preview_hunk)
         map('n', '<leader>hb', function() gs.blame_line { full = true } end)
-        map('n', '<leader>tb', gs.toggle_current_line_blame)
+        -- map('n', '<leader>tb', gs.toggle_current_line_blame)
         map('n', '<leader>hd', gs.diffthis)
         map('n', '<leader>hD', function() gs.diffthis('~') end)
         map('n', '<leader>td', gs.toggle_deleted)
