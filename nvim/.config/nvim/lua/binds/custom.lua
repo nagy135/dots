ae728ce727e5b8fe6c42327eb6c1b227c1dbbe02
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-05-29 12:22:22 +0200

    feat(nvim): key-menu custom scripts

--- nvim/.config/nvim/lua/binds/custom.lua
+++ nvim/.config/nvim/lua/binds/custom.lua
@@ -1,5 +1,9 @@
 require('scripts')
 
-vim.keymap.set('n', ';c', function() signature() end, { desc = 'Function signature' })
-vim.keymap.set('n', ';l', function() debug_log() end, { desc = 'Debug log' })
-vim.keymap.set('n', ';L', function() debug_log(true) end, { desc = 'Debug log (UP)' })
+vim.keymap.set('n', '<leader>;',
+    function() require 'key-menu'.open_window('<leader>;') end,
+    { desc = 'Custom scripts' })
+
+vim.keymap.set('n', '<leader>;c', function() signature() end, { desc = 'Function signature' })
+vim.keymap.set('n', '<leader>;l', function() debug_log() end, { desc = 'Debug log' })
+vim.keymap.set('n', '<leader>;L', function() debug_log(true) end, { desc = 'Debug log (UP)' })
