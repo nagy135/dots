8e5eb206c136162ecfdf6b761c11bb6a30242568
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-06-06 11:59:21 +0200

    feat(nvim): configurable options of indent guides

--- nvim/.config/nvim/lua/my_indent-blackline.lua
+++ nvim/.config/nvim/lua/my_indent-blackline.lua
@@ -1,21 +1,35 @@
-require("indent_blankline").setup {
-    char = "|",
-    buftype_exclude = {"terminal"}
+local OPTIONS = {
+    INDENT_GUIDES = 1,
+    VSCODE_YELLOW_LINE = 2,
+    BASIC = 3 -- uses default plugin options
 }
+-- EDIT HERE
+local mode = OPTIONS.INDENT_GUIDES;
+-- EDIT HERE
 
--- INDENT GUIDES ALTERNATIVE
--- vim.cmd [[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]]
---
--- require("indent_blankline").setup {
---     char = "",
---     char_highlight_list = {
---         "IndentBlanklineIndent1",
---         "IndentBlanklineIndent2",
---     },
---     space_char_highlight_list = {
---         "IndentBlanklineIndent1",
---         "IndentBlanklineIndent2",
---     },
---     show_trailing_blankline_indent = false,
--- }
+local opts = {}
+if mode == OPTIONS.INDENT_GUIDES then
+    vim.cmd [[highlight IndentBlanklineIndent1 guibg=#3a3a3a gui=nocombine]]
+    vim.cmd [[highlight IndentBlanklineIndent2 guibg=#2f2f2f gui=nocombine]]
+    opts = {
+        char = "",
+        char_highlight_list = {
+            "IndentBlanklineIndent1",
+            "IndentBlanklineIndent2",
+        },
+        space_char_highlight_list = {
+            "IndentBlanklineIndent1",
+            "IndentBlanklineIndent2",
+        },
+        show_trailing_blankline_indent = false,
+    }
+elseif mode == OPTIONS.VSCODE_YELLOW_LINE then
+    vim.cmd [[highlight IndentBlanklineContextChar guifg=#f9dc2b gui=nocombine]]
+    opts = {
+        show_current_context = true,
+        show_current_context_start = true,
+        char = "┆",
+        buftype_exclude = { "terminal" }
+    }
+end
+require("indent_blankline").setup(opts)
