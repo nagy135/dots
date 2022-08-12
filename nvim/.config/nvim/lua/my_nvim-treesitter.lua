f49244a7aaee1a35cd25b9d6f9b7714a644536c5
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-08-12 12:09:15 +0200

    feat(nvim): nvim-ts-rainbow custom colors (too dark red in defaults)

--- nvim/.config/nvim/lua/my_nvim-treesitter.lua
+++ nvim/.config/nvim/lua/my_nvim-treesitter.lua
@@ -1,4 +1,4 @@
-require'nvim-treesitter.configs'.setup {
+require 'nvim-treesitter.configs'.setup {
     ensure_installed = "all",
     ignore_install = { "kotlin" },
     highlight = {
@@ -26,7 +26,15 @@ require'nvim-treesitter.configs'.setup {
         -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
         extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
         max_file_lines = 1000, -- Do not enable for files with more than n lines, int
-        -- colors = {}, -- table of hex strings
+        colors = {
+            "#fb4934",
+            "#b16286",
+            "#eebd35",
+            "#689d6a",
+            "#d65d0e",
+            "#458588",
+            "#dedede",
+        },
         -- termcolors = {} -- table of colour name strings
     }
 }
