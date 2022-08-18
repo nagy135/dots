97efd21db94434210a738b5e068fbff071b36b39
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-08-18 12:22:33 +0200

    feat(nvim): lualine theme => gruvbox-baby

--- nvim/.config/nvim/lua/my_lualine.lua
+++ nvim/.config/nvim/lua/my_lualine.lua
@@ -1,7 +1,7 @@
 require'lualine'.setup {
   options = {
     icons_enabled = true,
-    theme = 'gruvbox-material',
+    theme = 'gruvbox-baby',
     section_separators = {left = '', right = ''},
     component_separators = {left = '', right = ''},
     disabled_filetypes = {'neo-tree'}
