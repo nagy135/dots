2a7124948c41ff0e6e180429908c9f32bfbc855f
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-08-30 11:19:03 +0200

    fix(nvim): also unbind lightspeed visual binds f/t

--- nvim/.config/nvim/lua/my_lightspeed.lua
+++ nvim/.config/nvim/lua/my_lightspeed.lua
@@ -5,3 +5,7 @@ vim.keymap.set('n', 'f', 'f');
 vim.keymap.set('n', 'F', 'F');
 vim.keymap.set('n', 't', 't');
 vim.keymap.set('n', 'T', 'T');
+vim.keymap.set('v', 'f', 'f');
+vim.keymap.set('v', 'F', 'F');
+vim.keymap.set('v', 't', 't');
+vim.keymap.set('v', 'T', 'T');
