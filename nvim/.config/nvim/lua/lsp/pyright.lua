478e0fc8baba0eb6eed2dad419ea4066eed15401
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-10-19 06:49:08 +0200

    fix(nvim): deprecated nvim-cmp capabilities fn

--- nvim/.config/nvim/lua/lsp/pyright.lua
+++ nvim/.config/nvim/lua/lsp/pyright.lua
@@ -1,1 +1,4 @@
-require'lspconfig'.pyright.setup{}
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+require 'lspconfig'.pyright.setup {
+    capabilities = capabilities
+}
--- nvim/.config/nvim/lua/lsp/tsserver.lua
+++ nvim/.config/nvim/lua/lsp/tsserver.lua
@@ -1,6 +1,6 @@
 -- sudo npm i typescript-language-server -g
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require'lspconfig'.tsserver.setup{
     capabilities = capabilities
