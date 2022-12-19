2468056b2af33756356249bc1d68469882f2b4dd
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-12-19 13:27:21 +0100

    feat(nvim): lualine material scheme

--- nvim/.config/nvim/lua/my_lualine.lua
+++ nvim/.config/nvim/lua/my_lualine.lua
@@ -1,7 +1,7 @@
 require'lualine'.setup {
   options = {
     icons_enabled = true,
-    theme = 'gruvbox-baby',
+    theme = 'gruvbox-material',
     section_separators = {left = '', right = ''},
     component_separators = {left = '', right = ''},
     disabled_filetypes = {'neo-tree'}
