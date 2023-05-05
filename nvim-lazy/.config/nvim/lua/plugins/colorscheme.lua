3427c185055ad1f40a23b7614accdd824f5aa1d3
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-05-05 06:21:21 +0200

    feat(nvim-lazy): init

---
+++ nvim-lazy/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox-material",
+    },
+  },
+  "ellisonleao/gruvbox.nvim",
+  "sainnhe/gruvbox-material",
+  "folke/tokyonight.nvim",
+  "olimorris/onedarkpro.nvim",
+  "sainnhe/gruvbox-material",
+  "rebelot/kanagawa.nvim",
+  "joshdick/onedark.vim",
+  "arturgoms/moonbow.nvim",
+}
