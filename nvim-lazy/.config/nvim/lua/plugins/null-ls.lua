7a31e42bb6e9d04143834582cca336841190d230
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-05-05 20:30:50 +0200

    feat(nvim-lazy): null-ls eslint

---
+++ nvim-lazy/.config/nvim/lua/plugins/null-ls.lua
@@ -0,0 +1,16 @@
+return {
+  "jose-elias-alvarez/null-ls.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = { "mason.nvim" },
+  opts = function()
+    local nls = require("null-ls")
+    return {
+      root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+      sources = {
+        nls.builtins.formatting.shfmt,
+        nls.builtins.diagnostics.eslint_d,
+        -- nls.builtins.diagnostics.flake8,
+      },
+    }
+  end,
+}
