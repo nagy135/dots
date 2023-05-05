3da32623ca47560beafbf23f536c155d3083e883
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-05-05 06:43:20 +0200

    feat(nvim-lazy): neo-tree vim binds

---
+++ nvim-lazy/.config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = {
+      mappings = {
+        ["<space>"] = "none",
+        ["s"] = "open_split",
+        ["v"] = "open_vsplit",
+      },
+    },
+  },
+}
