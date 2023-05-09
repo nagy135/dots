c069ae81834f47cfd635914933f84709b734ada6
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-05-09 13:40:16 +0200

    feat(nvim-lazy): telescope binds

--- nvim-lazy/.config/nvim/lua/plugins/telescope.lua
+++ nvim-lazy/.config/nvim/lua/plugins/telescope.lua
@@ -8,8 +8,10 @@ return {
         i = {
           ["<C-j>"] = telescope_actions.move_selection_next,
           ["<C-k>"] = telescope_actions.move_selection_previous,
-          -- ["<C-q>"] = require("telescope.actions").send_selected_to_qflist + require("telescope.actions").open_qflist,
-          -- ["<C-a>"] = require("telescope.actions").send_to_qflist + require("telescope.actions").open_qflist,
+          ["<C-s>"] = telescope_actions.select_horizontal,
+          ["<C-v>"] = telescope_actions.select_vertical,
+          ["<C-q>"] = require("telescope.actions").send_selected_to_qflist + require("telescope.actions").open_qflist,
+          ["<C-a>"] = require("telescope.actions").send_to_qflist + require("telescope.actions").open_qflist,
           -- ["<c-a>"] = require("trouble.providers.telescope").open_with_trouble,
           -- ["<c-q>"] = require("trouble.providers.telescope").open_selected_with_trouble,
         },
