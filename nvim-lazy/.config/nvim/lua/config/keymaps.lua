4260a072de4656e50b86dbe4a0c1364572f7f89b
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-05-13 21:22:32 +0200

    feat(nvim-lazy): qf binds, move line

--- nvim-lazy/.config/nvim/lua/config/keymaps.lua
+++ nvim-lazy/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set('n', 'c-k', '<CMD>cp<CR>', { desc = "Quickfix previous" })
+vim.keymap.set('n', 'c-j', '<CMD>cn<CR>', { desc = "Quickfix next" })
+vim.keymap.set('n', 'c-c', '<CMD>cc<CR>', { desc = "Quickfix close" })
+vim.keymap.set('n', ',j', '<CMD>move .+1<CR>==', { desc = "Move line down" })
+vim.keymap.set('n', ',k', '<CMD>move .-2<CR>==', { desc = "Move line up" })
