d1108f3f361546b1d62a2805ecfd8cf6e75c85b1
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-08-28 13:29:46 +0200

    feat(nvim): neo-tree

--- nvim-lazy/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim-lazy/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,6 +1,14 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    "s1n7ax/nvim-window-picker",
+  },
   opts = {
+    enable_normal_mode_for_inputs = true,
     window = {
       mappings = {
         ["<space>"] = "none",
