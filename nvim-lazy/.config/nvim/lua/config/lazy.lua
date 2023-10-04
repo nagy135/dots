1bf80295857676101e687c86065caa6802ce736f
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-10-04 14:26:19 +0200

    fix(nvim): flash is main now

--- nvim-lazy/.config/nvim/lua/config/lazy.lua
+++ nvim-lazy/.config/nvim/lua/config/lazy.lua
@@ -16,7 +16,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
-    { import = "lazyvim.plugins.extras.editor.flash" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
