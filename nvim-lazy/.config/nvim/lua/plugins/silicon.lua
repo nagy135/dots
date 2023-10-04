b74031861eea2bc86091a8bd423bdfaec9effb78
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-10-04 14:26:47 +0200

    feat(nvim): silicon theme

--- nvim-lazy/.config/nvim/lua/plugins/silicon.lua
+++ nvim-lazy/.config/nvim/lua/plugins/silicon.lua
@@ -1,3 +1,27 @@
 return {
   "segeljakt/vim-silicon",
+  keys = {
+    {
+      "<leader>cs",
+      "<cmd>Silicon<cr>",
+      desc = "Screenshot code"
+    }
+  },
+  config = function()
+    vim.cmd [[ let g:silicon = {
+      \   'theme':              'Visual Studio Dark+',
+      \   'font':               'Mononoki Nerd Font',
+      \   'background':         '#AAAAFF',
+      \   'shadow-color':       '#222222',
+      \   'line-pad':                   2,
+      \   'pad-horiz':                 80,
+      \   'pad-vert':                 100,
+      \   'shadow-blur-radius':        30,
+      \   'shadow-offset-x':            0,
+      \   'shadow-offset-y':            0,
+      \   'line-number':           v:true,
+      \   'round-corner':          v:true,
+      \   'window-controls':       v:true,
+      \ } ]]
+  end
 }
