f8e406bf44683b1e535bb6408d1cc8ba66f09df9
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-10-06 11:08:52 +0200

    feat(nvim): flash highlight fix for gruvbox-material

--- nvim-lazy/.config/nvim/lua/config/autocmds.lua
+++ nvim-lazy/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_hl(0, "FlashMatch", { fg = "#ea6962" })
+  end,
+}
+)
