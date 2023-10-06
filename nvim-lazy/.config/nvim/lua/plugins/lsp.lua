123728dd1815b67a843691534fd1b469cefd1102
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-10-06 11:09:20 +0200

    feat(nvim): inlayHints for future version

---
+++ nvim-lazy/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tsserver = {
+          settings = {
+            typescript = {
+              inlayHints = {
+                -- taken from https://github.com/typescript-language-server/typescript-language-server#workspacedidchangeconfiguration
+                includeInlayEnumMemberValueHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayParameterNameHints = 'all',
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true, -- false
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayVariableTypeHintsWhenTypeMatchesName = true -- false
+              }
+            },
+            javascript = {
+              inlayHints = {
+                includeInlayEnumMemberValueHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayParameterNameHints = 'all',
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayVariableTypeHintsWhenTypeMatchesName = true
+              }
+            },
+          },
+        },
+      },
+    },
+  }
+}
