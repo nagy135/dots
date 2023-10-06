bc5228f4af935d1106587c987981bb7148387082
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-10-06 11:08:10 +0200

    feat(nvim): neorg init

---
+++ nvim-lazy/.config/nvim/lua/plugins/neorg.lua
@@ -0,0 +1,60 @@
+local wk = require("which-key")
+wk.register({
+  ["<leader>n"] = { name = "+neorg" },
+})
+wk.register({
+  ["<leader>np"] = { name = "+presenter" },
+})
+
+return {
+  "nvim-neorg/neorg",
+  build = ":Neorg sync-parsers",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    require("neorg").setup {
+      load = {
+        ["core.defaults"] = {}, -- Loads default behaviour
+
+        ["core.integrations.treesitter"] = {},
+        ["core.syntax"] = {},
+        ["core.mode"] = {},
+        ["core.presenter"] = {
+          config = {
+            zen_mode = "zen-mode"
+          }
+        },
+        ["core.queries.native"] = {},
+        ["core.ui"] = {},
+
+        ["core.concealer"] = {}, -- Adds pretty icons to your documents
+        ["core.dirman"] = {      -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              notes = "~/notes",
+            },
+            default_workspace = "notes",
+          },
+        },
+      },
+    }
+  end,
+  keys = {
+    -- add a keymap to browse plugin files
+    -- stylua: ignore
+    {
+      "<leader>ni",
+      "<cmd>Neorg index<cr>",
+      desc = "Index",
+    },
+    {
+      "<leader>nps",
+      "<cmd>Neorg presenter start<cr>",
+      desc = "Start",
+    },
+    {
+      "<leader>npc",
+      "<cmd>Neorg presenter close<cr>",
+      desc = "Close",
+    },
+  },
+}
