1f8d068b545a097e3ce78b6ebf73ea2968758f26
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-10-18 19:11:28 +0200

    fix(nvim): disable silicon binds

--- nvim-lazy/.config/nvim/lua/plugins/silicon.lua
+++ nvim-lazy/.config/nvim/lua/plugins/silicon.lua
@@ -1,36 +1,36 @@
 return {
   "segeljakt/vim-silicon",
   cmd = { "Silicon" },
-  keys = {
-    {
-      "<leader>cs",
-      "<cmd>Silicon<cr>",
-      desc = "Screenshot code",
-      mode = { "n" }
-    },
-    {
-      "<leader>cs",
-      "<cmd>'<,'>Silicon<cr>",
-      desc = "Screenshot highlight",
-      mode = { "v", "s", "x" }
-    }
-  },
+  -- keys = {
+  --   {
+  --     "<leader>cs",
+  --     "<cmd>Silicon<cr>",
+  --     desc = "Screenshot code",
+  --     mode = { "n" }
+  --   },
+  --   {
+  --     "<leader>cs",
+  --     "<cmd>'<,'>Silicon<cr>",
+  --     desc = "Screenshot highlight",
+  --     mode = { "v", "s", "x" }
+  --   }
+  -- },
   config = function()
     vim.g.silicon = {
-      ['theme'] = 'Visual Studio Dark+',
-      ['font'] = 'Mononoki Nerd Font',
-      ['background'] = '#AAAAFF',
-      ['shadow-color'] = '#222222',
-      ['line-pad'] = 2,
-      ['pad-horiz'] = 80,
-      ['pad-vert'] = 100,
-      ['shadow-blur-radius'] = 30,
-      ['shadow-offset-x'] = 0,
-      ['shadow-offset-y'] = 0,
-      ['line-number'] = true,
-      ['round-corner'] = true,
-      ['window-controls'] = true,
-      ['output'] = '~/Pictures/silicon',
+      ["theme"] = "Visual Studio Dark+",
+      ["font"] = "Mononoki Nerd Font",
+      ["background"] = "#AAAAFF",
+      ["shadow-color"] = "#222222",
+      ["line-pad"] = 2,
+      ["pad-horiz"] = 80,
+      ["pad-vert"] = 100,
+      ["shadow-blur-radius"] = 30,
+      ["shadow-offset-x"] = 0,
+      ["shadow-offset-y"] = 0,
+      ["line-number"] = true,
+      ["round-corner"] = true,
+      ["window-controls"] = true,
+      ["output"] = "~/Pictures/silicon",
     }
-  end
+  end,
 }
