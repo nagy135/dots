edb5781b9665c10a2f8c5755e8d70a8c2cc28fba
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-10-18 10:59:17 +0200

    feat(nvim): binds for lsp

--- nvim-lazy/.config/nvim/lua/plugins/lsp.lua
+++ nvim-lazy/.config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,33 @@
+local wk = require("which-key")
+wk.register({
+  ["<leader>cl"] = { name = "+lsp" },
+})
+
 return {
   {
     "neovim/nvim-lspconfig",
+    keys = {
+      {
+        "<leader>cll",
+        "<CMD>LspInfo<CR>",
+        desc = "Info",
+      },
+      {
+        "<leader>clc",
+        "<CMD>LspStop<CR>",
+        desc = "Stop (Cancel)",
+      },
+      {
+        "<leader>cls",
+        "<CMD>LspStart<CR>",
+        desc = "Start",
+      },
+      {
+        "<leader>clr",
+        "<CMD>LspRestart<CR>",
+        desc = "Restart",
+      },
+    },
     opts = {
       servers = {
         tsserver = {
@@ -11,28 +38,28 @@ return {
                 includeInlayEnumMemberValueHints = true,
                 includeInlayFunctionLikeReturnTypeHints = true,
                 includeInlayFunctionParameterTypeHints = true,
-                includeInlayParameterNameHints = 'all',
+                includeInlayParameterNameHints = "all",
                 includeInlayParameterNameHintsWhenArgumentMatchesName = true, -- false
                 includeInlayPropertyDeclarationTypeHints = true,
                 includeInlayVariableTypeHints = true,
-                includeInlayVariableTypeHintsWhenTypeMatchesName = true -- false
-              }
+                includeInlayVariableTypeHintsWhenTypeMatchesName = true, -- false
+              },
             },
             javascript = {
               inlayHints = {
                 includeInlayEnumMemberValueHints = true,
                 includeInlayFunctionLikeReturnTypeHints = true,
                 includeInlayFunctionParameterTypeHints = true,
-                includeInlayParameterNameHints = 'all',
+                includeInlayParameterNameHints = "all",
                 includeInlayParameterNameHintsWhenArgumentMatchesName = true,
                 includeInlayPropertyDeclarationTypeHints = true,
                 includeInlayVariableTypeHints = true,
-                includeInlayVariableTypeHintsWhenTypeMatchesName = true
-              }
+                includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+              },
             },
           },
         },
       },
     },
-  }
+  },
 }
