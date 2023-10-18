e1594dbbd19118d414513782ca064f5849ae56d6
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-10-18 19:10:32 +0200

    feat(nvim): symbols outline

--- nvim-lazy/.config/nvim/lua/config/lazy.lua
+++ nvim-lazy/.config/nvim/lua/config/lazy.lua
@@ -10,7 +10,6 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim",                                  import = "lazyvim.plugins" },
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
@@ -19,6 +18,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.test.core" },
+    { import = "lazyvim.plugins.extras.editor.symbols-outline" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
