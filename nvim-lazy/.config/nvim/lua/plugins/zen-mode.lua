3c8e12be3652722154144768397ef5b37359ff1b
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-10-22 20:43:48 +0200

    feat(nvim): zen-mode bind

--- nvim-lazy/.config/nvim/lua/plugins/zen-mode.lua
+++ nvim-lazy/.config/nvim/lua/plugins/zen-mode.lua
@@ -2,7 +2,16 @@ return {
   "folke/zen-mode.nvim",
   opts = {
     window = {
-      width = .6
-    }
-  }
+      width = 0.6,
+    },
+  },
+  keys = {
+    {
+      "<leader><leader>z",
+      function()
+        require("zen-mode").toggle()
+      end,
+      desc = "Zen Mode",
+    },
+  },
 }
