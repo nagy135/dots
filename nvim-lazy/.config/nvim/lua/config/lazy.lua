9b41eb403b74b30b4b1c8a0422f457129ca4d1ef
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-10-22 20:43:20 +0200

    refactor(nvim): lang order extras

--- nvim-lazy/.config/nvim/lua/config/lazy.lua
+++ nvim-lazy/.config/nvim/lua/config/lazy.lua
@@ -13,8 +13,8 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.test.core" },
