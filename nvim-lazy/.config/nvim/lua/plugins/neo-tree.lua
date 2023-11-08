2f92c1707a8551920c6256dc412f49dfe089b5b5
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-11-08 14:50:48 +0100

    feat(nvim): neo-tree Y bind to copy name of file with picker

--- nvim-lazy/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim-lazy/.config/nvim/lua/plugins/neo-tree.lua
@@ -15,6 +15,38 @@ return {
         ["s"] = "split_with_window_picker",
         ["v"] = "vsplit_with_window_picker",
         ["/"] = "none",
+
+        ["Y"] = function(state)
+          -- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
+          -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
+          local node = state.tree:get_node()
+          local filepath = node:get_id()
+          local filename = node.name
+          local modify = vim.fn.fnamemodify
+
+          local results = {
+            filepath,
+            modify(filepath, ":."),
+            modify(filepath, ":~"),
+            filename,
+            modify(filename, ":r"),
+            modify(filename, ":e"),
+          }
+
+          vim.ui.select({
+            "1. Absolute path: " .. results[1],
+            "2. Path relative to CWD: " .. results[2],
+            "3. Path relative to HOME: " .. results[3],
+            "4. Filename: " .. results[4],
+            "5. Filename without extension: " .. results[5],
+            "6. Extension of the filename: " .. results[6],
+          }, { prompt = "Choose to copy to clipboard:" }, function(choice)
+            local i = tonumber(choice:sub(1, 1))
+            local result = results[i]
+            vim.fn.setreg("+", result)
+            vim.notify("Copied: " .. result)
+          end)
+        end,
       },
     },
   },
