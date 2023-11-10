b76e6e8f94d02f01fe07d34f34827346281f873f
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-11-10 10:56:50 +0100

    feat(nvim): twilight toggle bind

--- nvim-lazy/.config/nvim/lua/plugins/twilight.lua
+++ nvim-lazy/.config/nvim/lua/plugins/twilight.lua
@@ -1,3 +1,12 @@
 return {
   "folke/twilight.nvim",
+  keys = {
+    {
+      "<leader>zt",
+      function()
+        require("twilight").toggle()
+      end,
+      desc = "Twilight (toggle)",
+    },
+  },
 }
