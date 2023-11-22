8157296182a3b9329101332357e88508e47a3ca4
Viktor Nagy <viktor.nagy1995@gmail.com>
2023-11-22 18:05:10 +0100

    feat(nvim): abbrev for closing

--- nvim-lazy/.config/nvim/lua/config/options.lua
+++ nvim-lazy/.config/nvim/lua/config/options.lua
@@ -2,3 +2,16 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.foldmethod = "marker"
+
+vim.cmd([[ cnoreabbrev W w ]])
+vim.cmd([[ cnoreabbrev Q q ]])
+
+vim.cmd([[ cnoreabbrev Wq wq ]])
+vim.cmd([[ cnoreabbrev wQ wq ]])
+
+vim.cmd([[ cnoreabbrev wQa wqa ]])
+vim.cmd([[ cnoreabbrev Wqa wqa ]])
+vim.cmd([[ cnoreabbrev wqA wqa ]])
+vim.cmd([[ cnoreabbrev WQa wqa ]])
+vim.cmd([[ cnoreabbrev wQA wqa ]])
+vim.cmd([[ cnoreabbrev WQA wqa ]])
