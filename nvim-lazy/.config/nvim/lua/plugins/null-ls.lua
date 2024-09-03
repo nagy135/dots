3353d7ba23481c38ab0540d50e3627801da24782
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-09-03 15:42:16 +0200

    feat(nvim): prettier

--- nvim-lazy/.config/nvim/lua/plugins/null-ls.lua
+++ nvim-lazy/.config/nvim/lua/plugins/null-ls.lua
@@ -2,16 +2,9 @@ return {
   "nvimtools/none-ls.nvim",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = { "mason.nvim" },
-  opts = function()
+  opts = function(_, opts)
     local nls = require("null-ls")
-    return {
-      root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-      sources = {
-        nls.builtins.formatting.shfmt,
-        -- nls.builtins.diagnostics.eslint_d,
-        nls.builtins.diagnostics.flake8,
-        nls.builtins.formatting.prettierd,
-      },
-    }
+    opts.sources = opts.sources or {}
+    table.insert(opts.sources, nls.builtins.formatting.prettier)
   end,
 }
