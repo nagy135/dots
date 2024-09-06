362980b2b3bf2609b4150d875e3eb15192697c7d
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-09-06 20:08:27 +0200

    feat(nvim): supermaven instead of copilot

--- nvim-lazy/.config/nvim/lua/plugins/copilot.lua
+++ nvim-lazy/.config/nvim/lua/plugins/copilot.lua
@@ -1,13 +1,35 @@
+-- return {
+--   "github/copilot.vim",
+--   config = function()
+--     vim.g.copilot_enabled = true
+--     vim.g.copilot_no_tab_map = true
+--     vim.cmd('imap <silent><script><expr> <C-l> copilot#Accept("")')
+--     vim.cmd([[
+-- 			let g:copilot_filetypes = {
+-- 	       \ 'TelescopePrompt': v:false,
+-- 	     \ }
+-- 			]])
+--   end,
+-- }
 return {
-  "github/copilot.vim",
-  config = function()
-    vim.g.copilot_enabled = true
-    vim.g.copilot_no_tab_map = true
-    vim.cmd('imap <silent><script><expr> <C-l> copilot#Accept("")')
-    vim.cmd([[
-			let g:copilot_filetypes = {
-	       \ 'TelescopePrompt': v:false,
-	     \ }
-			]])
-  end,
+  {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup({})
+    end,
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    keys = {
+      { "<tab>", false, mode = { "i", "s" } },
+      { "<s-tab>", false, mode = { "i", "s" } },
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    keys = {
+      { "<tab>", false, mode = { "i", "s" } },
+      { "<s-tab>", false, mode = { "i", "s" } },
+    },
+  },
 }
