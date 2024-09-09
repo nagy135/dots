5ab34f646d0af305f54c2137a5c662436c4a150f
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-09-09 23:00:55 +0200

    feat(nvim): prettier extra

--- nvim-lazy/.config/nvim/lua/config/lazy.lua
+++ nvim-lazy/.config/nvim/lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.rest" },
     { import = "lazyvim.plugins.extras.editor.mini-files" },
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.test.core" },
