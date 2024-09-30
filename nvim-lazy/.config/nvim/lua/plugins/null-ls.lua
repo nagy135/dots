3428edd7c907548ac68b1358b5b3b90a51276a34
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-09-30 20:26:21 +0200

    feat(nvim): disable prettier plugin and use manual prettierd

--- nvim-lazy/.config/nvim/lua/plugins/null-ls.lua
+++ nvim-lazy/.config/nvim/lua/plugins/null-ls.lua
@@ -5,6 +5,6 @@ return {
   opts = function(_, opts)
     local nls = require("null-ls")
     opts.sources = opts.sources or {}
-    table.insert(opts.sources, nls.builtins.formatting.prettier)
+    table.insert(opts.sources, nls.builtins.formatting.prettierd)
   end,
 }
