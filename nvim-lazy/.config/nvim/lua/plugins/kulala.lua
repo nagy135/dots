5de6c1d5192609db28b1637eb50bfadc64eb9778
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-10-10 14:26:05 +0200

    feat(nvim): more kulala binds

--- nvim-lazy/.config/nvim/lua/plugins/kulala.lua
+++ nvim-lazy/.config/nvim/lua/plugins/kulala.lua
@@ -2,5 +2,9 @@ return {
   "mistweaverco/kulala.nvim",
   keys = {
     { "<leader>Rf", "<cmd>lua require('kulala').search()<cr>", desc = "Search" },
+    { "<leader>Rr", "<cmd>lua require('kulala').replay()<cr>", desc = "Replay" },
+    { "<leader>Ri", "<cmd>lua require('kulala').inspect()<cr>", desc = "Inspect" },
+    { "<leader>Rc", "<cmd>lua require('kulala').copy()<cr>", desc = "Copy (curl)" },
+    { "<leader>RC", "<cmd>lua require('kulala').copy()<cr>", desc = "Create from curl" },
   },
 }
