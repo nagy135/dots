2c57b332174b83444ac4e37f74ee8f5176d7aa3e
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-11-12 23:01:39 +0100

    feat(nvim): clear project wiki

---
+++ nvim-lazy/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return {
+        --[[add your custom lualine config here]]
+      }
+    end,
+  },
+}
