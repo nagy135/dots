a6a0f2805b12abd3332f02c523935cfbddb1e58a
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-11-26 11:34:49 +0100

    feat(nvim): kulala toggle headers bind

--- nvim-lazy/.config/nvim/lua/plugins/kulala.lua
+++ nvim-lazy/.config/nvim/lua/plugins/kulala.lua
@@ -4,6 +4,7 @@ return {
     { "<leader>Rf", "<cmd>lua require('kulala').search()<cr>", desc = "Search" },
     { "<leader>Rr", "<cmd>lua require('kulala').replay()<cr>", desc = "Replay" },
     { "<leader>Ri", "<cmd>lua require('kulala').inspect()<cr>", desc = "Inspect" },
+    { "<leader>Rt", "<cmd>lua require('kulala').toggle_view()<cr>", desc = "Toggle view" },
     { "<leader>Rc", "<cmd>lua require('kulala').copy()<cr>", desc = "Copy (curl)" },
     { "<leader>RC", "<cmd>lua require('kulala').copy()<cr>", desc = "Create from curl" },
   },
