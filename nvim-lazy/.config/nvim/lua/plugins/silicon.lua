02e4dc637e0a971c91a30d3ec1629d6efe7fb489
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-12-07 20:53:11 +0100

    fix(nvim): silicon bind

--- nvim-lazy/.config/nvim/lua/plugins/silicon.lua
+++ nvim-lazy/.config/nvim/lua/plugins/silicon.lua
@@ -1,36 +1,28 @@
 return {
-  "segeljakt/vim-silicon",
-  cmd = { "Silicon" },
-  -- keys = {
-  --   {
-  --     "<leader>cs",
-  --     "<cmd>Silicon<cr>",
-  --     desc = "Screenshot code",
-  --     mode = { "n" }
-  --   },
-  --   {
-  --     "<leader>cs",
-  --     "<cmd>'<,'>Silicon<cr>",
-  --     desc = "Screenshot highlight",
-  --     mode = { "v", "s", "x" }
-  --   }
-  -- },
-  config = function()
-    vim.g.silicon = {
-      ["theme"] = "Visual Studio Dark+",
-      ["font"] = "Mononoki Nerd Font",
-      ["background"] = "#AAAAFF",
-      ["shadow-color"] = "#222222",
-      ["line-pad"] = 2,
-      ["pad-horiz"] = 80,
-      ["pad-vert"] = 100,
-      ["shadow-blur-radius"] = 30,
-      ["shadow-offset-x"] = 0,
-      ["shadow-offset-y"] = 0,
-      ["line-number"] = true,
-      ["round-corner"] = true,
-      ["window-controls"] = true,
-      ["output"] = "~/Pictures/silicon",
-    }
-  end,
+  "michaelrommel/nvim-silicon",
+  lazy = true,
+  cmd = "Silicon",
+  main = "nvim-silicon",
+  opts = {
+    -- Configuration here, or leave empty to use defaults
+    line_offset = function(args)
+      return args.line1
+    end,
+  },
+  keys = {
+    {
+      "<leader>cs",
+      function()
+        require("nvim-silicon").clip()
+      end,
+      desc = "Screenshot code",
+      mode = { "n" },
+    },
+    {
+      "<leader>cs",
+      "<cmd>'<,'>Silicon<cr>",
+      desc = "Screenshot highlight",
+      mode = { "v", "s", "x" },
+    },
+  },
 }
