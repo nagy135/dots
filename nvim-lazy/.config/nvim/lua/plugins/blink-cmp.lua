9e4852d25b7345790c443e407d3a34bd29f551e8
Viktor Nagy <viktor.nagy1995@gmail.com>
2025-01-14 19:12:40 +0100

    feat(nvim): blink.cmp

--- nvim-lazy/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim-lazy/.config/nvim/lua/plugins/blink-cmp.lua
@@ -1,20 +1,44 @@
+-- return {
+--   "saghen/blink.cmp",
+--   dependencies = { "supermaven-nvim", "saghen/blink.compat" },
+--   opts = {
+--     keymap = {
+--       preset = "default",
+--       ["<C-p>"] = { "select_prev", "fallback" },
+--       ["<C-n>"] = { "select_next", "fallback" },
+--       ["<C-e>"] = { "hide" },
+--       ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+--       ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+--       ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+--     },
+--     providers = {
+--       buffer = {
+--         enabled = false,
+--       },
+--     },
+--   },
+-- }
 return {
   "saghen/blink.cmp",
-  dependencies = { "supermaven-nvim", "saghen/blink.compat" },
+  dependencies = "rafamadriz/friendly-snippets",
   opts = {
-    keymap = {
-      preset = "default",
-      ["<C-p>"] = { "select_prev", "fallback" },
-      ["<C-n>"] = { "select_next", "fallback" },
-      ["<C-e>"] = { "hide" },
-      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-      ["<C-u>"] = { "scroll_documentation_up", "fallback" },
-      ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+    keymap = { preset = "default" },
+
+    appearance = {
+      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+      -- Useful for when your theme doesn't support blink.cmp
+      -- Will be removed in a future release
+      -- use_nvim_cmp_as_default = true,
+      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = "mono",
     },
-    providers = {
-      buffer = {
-        enabled = false,
-      },
+
+    -- Default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
     },
   },
+  opts_extend = { "sources.default" },
 }
