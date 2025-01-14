6f569fd0baf5abdc3bc6538437b44a575293e4b1
Viktor Nagy <viktor.nagy1995@gmail.com>
2025-01-14 19:13:05 +0100

    feat(nvim): telescope ripgrep custom searcher

--- nvim-lazy/.config/nvim/lua/plugins/telescope.lua
+++ nvim-lazy/.config/nvim/lua/plugins/telescope.lua
@@ -1,4 +1,53 @@
 local telescope_actions = require("telescope.actions")
+
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local sorters = require("telescope.sorters")
+local make_entry = require("telescope.make_entry")
+local conf = require("telescope.config").values
+
+local ripgrep = function(opts)
+  opts = opts or {}
+  opts.cwd = opts.cwd or vim.uv.cwd()
+  local finder = finders.new_async_job({
+    command_generator = function(prompt)
+      if not prompt or prompt == "" then
+        return nil
+      end
+
+      local pieces = vim.split(prompt, "  ")
+      local args = { "rg" }
+      if pieces[1] then
+        table.insert(args, "-e")
+        table.insert(args, pieces[1])
+      end
+
+      if pieces[2] then
+        table.insert(args, "-g")
+        table.insert(args, pieces[2])
+      end
+      table.insert(args, "--color=never")
+      table.insert(args, "--no-heading")
+      table.insert(args, "--with-filename")
+      table.insert(args, "--line-number")
+      table.insert(args, "--column")
+      table.insert(args, "--smart-case")
+      return args
+    end,
+    entry_makers = make_entry.gen_from_vimgrep(opts),
+    cwd = opts.cwd,
+  })
+  pickers
+    .new(opts, {
+      debounce = 100,
+      prompt_title = "ripgrep",
+      finder = finder,
+      previewer = conf.grep_previewer(opts),
+      sorter = sorters.empty(),
+    })
+    :find()
+end
+
 return {
   "nvim-telescope/telescope.nvim",
   -- change some options
@@ -60,6 +109,11 @@ return {
         require("telescope.builtin").find_files({ cwd = "%:h" })
       end,
       desc = "Find file in subtree",
+    },
+    {
+      "<leader>f/",
+      ripgrep,
+      desc = "Ripgrep",
     },
   },
 }
