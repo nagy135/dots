9dcb74c805c345e30a14a6bface9d170204bf252
Viktor Nagy <viktor.nagy1995@gmail.com>
2025-03-04 21:43:42 +0100

    feat(nvim): avante disable hint

--- nvim-lazy/.config/nvim/lua/plugins/avante.lua
+++ nvim-lazy/.config/nvim/lua/plugins/avante.lua
@@ -4,7 +4,7 @@ return {
   lazy = false,
   version = false, -- set this if you want to always pull the latest change
   opts = {
-    -- add any opts here
+    hints = { enabled = false },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
