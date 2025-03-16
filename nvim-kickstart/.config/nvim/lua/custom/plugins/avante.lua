e8a4d05e2fe184ac75f48983ef273cbe404a0a35
Viktor Nagy <viktor.nagy1995@gmail.com>
2025-03-16 02:02:02 +0100

    feat(nvim-kickstart): avante

---
+++ nvim-kickstart/.config/nvim/lua/custom/plugins/avante.lua
@@ -0,0 +1,62 @@
+return {
+  'yetone/avante.nvim',
+  event = 'VeryLazy',
+  lazy = false,
+  version = false, -- set this if you want to always pull the latest change
+  opts = {
+    hints = { enabled = false },
+    -- claude = {
+    --   disabled_tools = { "python" },
+    -- },
+    provider = 'openai',
+    openai = {
+      endpoint = 'https://api.openai.com/v1',
+      model = 'gpt-4o', -- your desired model (or use gpt-4o, etc.)
+      timeout = 30000, -- timeout in milliseconds
+      temperature = 0, -- adjust if needed
+      max_tokens = 4096,
+      -- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
+    },
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = 'make',
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'stevearc/dressing.nvim',
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+    --- The below dependencies are optional,
+    'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+    'zbirenbaum/copilot.lua', -- for providers='copilot'
+    {
+      -- support for image pasting
+      'HakonHarnes/img-clip.nvim',
+      event = 'VeryLazy',
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { 'markdown', 'Avante' },
+      },
+      ft = { 'markdown', 'Avante' },
+      keys = {
+        { '<leader>mr', '<cmd>RenderMarkdown toggle<cr>', desc = 'Render Markdown (toggle)' },
+        { '<leader>rm', '<cmd>RenderMarkdown toggle<cr>', desc = 'Render Markdown (toggle)' },
+      },
+    },
+  },
+}
