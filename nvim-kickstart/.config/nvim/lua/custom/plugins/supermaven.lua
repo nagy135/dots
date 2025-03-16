79bff0a1b2fa846d0ba261c9aadd591d744a017b
Viktor Nagy <viktor.nagy1995@gmail.com>
2025-03-16 02:02:07 +0100

    feat(nvim-kickstart): supermaven

---
+++ nvim-kickstart/.config/nvim/lua/custom/plugins/supermaven.lua
@@ -0,0 +1,24 @@
+return {
+  'supermaven-inc/supermaven-nvim',
+  lazy = false,
+  config = function()
+    require('supermaven-nvim').setup {
+      color = {
+        suggestion_color = '#DBDBDB',
+        cterm = 244,
+      },
+    }
+  end,
+  keys = {
+    {
+      '<leader>ct',
+      function()
+        local api = require 'supermaven-nvim.api'
+        local is_running = api.is_running()
+        api.toggle()
+        vim.notify('SuperMaven ' .. (is_running and 'disabled' or 'enabled'), is_running and 'warn' or 'info')
+      end,
+      desc = 'Toggle SuperMaven',
+    },
+  },
+}
