6aeca04cc7bb5e25e22ed508e4443819327f05c6
Viktor Nagy <viktor.nagy1995@gmail.com>
2025-05-24 23:10:52 +0200

    feat(nvim): undotree

---
+++ nvim-kickstart/.config/nvim/lua/custom/plugins/undotree.lua
@@ -0,0 +1,7 @@
+return {
+  'mbbill/undotree',
+  keys = {
+    { '<leader>uu', '<cmd>UndotreeToggle<cr>', desc = 'Undotree (toggle)' },
+    { '<leader>uf', '<cmd>UndotreeFocus<cr>', desc = 'Undotree (focus)' },
+  },
+}
