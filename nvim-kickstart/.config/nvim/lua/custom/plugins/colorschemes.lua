544e5847682dbee01470d0541c78421bc2397898
Viktor Nagy <viktor.nagy1995@gmail.com>
2025-08-10 14:17:58 +0200

    feat(nvim): vague theme

--- nvim-kickstart/.config/nvim/lua/custom/plugins/colorschemes.lua
+++ nvim-kickstart/.config/nvim/lua/custom/plugins/colorschemes.lua
@@ -10,4 +10,16 @@ return {
     'rebelot/kanagawa.nvim',
   },
   { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+  { 'shaunsingh/nord.nvim' },
+  {
+    'vague2k/vague.nvim',
+    -- lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    -- priority = 1000, -- make sure to load this before all the other plugins
+    -- config = function()
+    --   require('vague').setup {
+    --     -- optional configuration here
+    --   }
+    --   vim.cmd 'colorscheme vague'
+    -- end,
+  },
 }
