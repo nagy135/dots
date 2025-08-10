8de547b8883daaa05f4a575814696f49adc28dda
Viktor Nagy <viktor.nagy1995@gmail.com>
2025-08-10 14:17:53 +0200

    feat(nvim): diffview toggle binds

--- nvim-kickstart/.config/nvim/lua/custom/plugins/diffview.lua
+++ nvim-kickstart/.config/nvim/lua/custom/plugins/diffview.lua
@@ -1,3 +1,31 @@
+local diffview_open = false
 return {
   'sindrets/diffview.nvim',
+  keys = {
+    {
+      '<leader>df',
+      function()
+        if diffview_open then
+          require('diffview').close()
+          diffview_open = false
+        else
+          require('diffview').file_history()
+          diffview_open = true
+        end
+      end,
+    },
+    {
+      '<leader>dt',
+      function()
+        if diffview_open then
+          require('diffview').close()
+          diffview_open = false
+        else
+          require('diffview').open {}
+          diffview_open = true
+        end
+      end,
+      desc = 'Toggle Diff View',
+    },
+  },
 }
