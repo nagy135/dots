0d6499e4abb786180944cec7b66436082bb9adb5
Viktor Nagy <viktor.nagy1995@gmail.com>
2025-09-24 11:08:30 +0200

    feat(nvim): debug macros with single quote

--- nvim-kickstart/.config/nvim/ftplugin/typescript.lua
+++ nvim-kickstart/.config/nvim/ftplugin/typescript.lua
@@ -3,25 +3,25 @@
 
 -- nnoremap <leader>p oconsole.log();<ESC>hi
 -- nnoremap <leader>P Oconsole.log();<ESC>hi
-vim.keymap.set('n', '<leader>;p', 'yiwoconsole.log("", );<ESC>hPF"P<ESC>', { desc = 'variable' })
-vim.keymap.set('n', '<leader>;P', 'yiwOconsole.log("", );<ESC>hPF"P<ESC>', { desc = 'variable (above)' })
-vim.keymap.set('n', '<leader>;;p', 'yiwoconsole.log("================\\n", "", , "\\n================");<ESC>F,PF"Pa: <ESC>', { desc = 'variable with lines' })
+vim.keymap.set('n', '<leader>;p', "yiwoconsole.log('', );<ESC>hPF'P<ESC>", { desc = 'variable' })
+vim.keymap.set('n', '<leader>;P', "yiwOconsole.log('', );<ESC>hPF'P<ESC>", { desc = 'variable (above)' })
+vim.keymap.set('n', '<leader>;;p', "yiwoconsole.log('================\\n', '', , '\\n================');<ESC>F,PF'Pa: <ESC>", { desc = 'variable with lines' })
 vim.keymap.set(
   'n',
   '<leader>;;P',
-  'yiwOconsole.log("================\\n", "", , "\\n================");<ESC>F,PF"Pa: <ESC>',
+  "yiwOconsole.log('================\\n', '', , '\\n================');<ESC>F,PF'Pa: <ESC>",
   { desc = 'variable with lines (above)' }
 )
 vim.keymap.set(
   'n',
   '<leader>;;j',
-  'yiwoconsole.log("================\\n", "", , "\\n================");<ESC>F,PF"Pa: <ESC>f,laJSON.stringify(<ESC>f,i, null, 2)<ESC>',
+  "yiwoconsole.log('================\\n', '', , '\\n================');<ESC>F,PF'Pa: <ESC>f,laJSON.stringify(<ESC>f,i, null, 2)<ESC>",
   { desc = 'JSON.stringify variable with lines' }
 )
 vim.keymap.set(
   'n',
   '<leader>;;J',
-  'yiwOconsole.log("================\\n", "", , "\\n================");<ESC>F,PF"Pa: <ESC>f,laJSON.stringify(<ESC>f,i, null, 2)<ESC>',
+  "yiwOconsole.log('================\\n', '', , '\\n================');<ESC>F,PF'Pa: <ESC>f,laJSON.stringify(<ESC>f,i, null, 2)<ESC>",
   { desc = 'JSON.stringify variable with lines (above)' }
 )
 -- vim.keymap.set("n", "<leader>r", "<CMD>!npm run start %<CR>", { desc = "npm run start" })
--- nvim-kickstart/.config/nvim/ftplugin/typescriptreact.lua
+++ nvim-kickstart/.config/nvim/ftplugin/typescriptreact.lua
@@ -1,22 +1,21 @@
-vim.keymap.set('n', '<leader>;p', 'yiwoconsole.log("", );<ESC>hPF"P<ESC>', { desc = 'variable' })
-vim.keymap.set('n', '<leader>;P', 'yiwOconsole.log("", );<ESC>hPF"P<ESC>', { desc = 'variable (above)' })
-vim.keymap.set('n', '<leader>;;p', 'yiwoconsole.log("================\\n", "", , "\\n================");<ESC>F,PF"Pa: <ESC>', { desc = 'variable with lines' })
+vim.keymap.set('n', '<leader>;p', "yiwoconsole.log('', );<ESC>hPF'P<ESC>", { desc = 'variable' })
+vim.keymap.set('n', '<leader>;P', "yiwOconsole.log('', );<ESC>hPF'P<ESC>", { desc = 'variable (above)' })
+vim.keymap.set('n', '<leader>;;p', "yiwoconsole.log('================\\n', '', , '\\n================');<ESC>F,PF'Pa: <ESC>", { desc = 'variable with lines' })
 vim.keymap.set(
   'n',
   '<leader>;;P',
-  'yiwOconsole.log("================\\n", "", , "\\n================");<ESC>F,PF"Pa: <ESC>',
+  "yiwOconsole.log('================\\n', '', , '\\n================');<ESC>F,PF'Pa: <ESC>",
   { desc = 'variable with lines (above)' }
 )
-
 vim.keymap.set(
   'n',
   '<leader>;;j',
-  'yiwoconsole.log("================\\n", "", , "\\n================");<ESC>F,PF"Pa: <ESC>f,laJSON.stringify(<ESC>f,i, null, 2)<ESC>',
+  "yiwoconsole.log('================\\n', '', , '\\n================');<ESC>F,PF'Pa: <ESC>f,laJSON.stringify(<ESC>f,i, null, 2)<ESC>",
   { desc = 'JSON.stringify variable with lines' }
 )
 vim.keymap.set(
   'n',
   '<leader>;;J',
-  'yiwOconsole.log("================\\n", "", , "\\n================");<ESC>F,PF"Pa: <ESC>f,laJSON.stringify(<ESC>f,i, null, 2)<ESC>',
+  "yiwOconsole.log('================\\n', '', , '\\n================');<ESC>F,PF'Pa: <ESC>f,laJSON.stringify(<ESC>f,i, null, 2)<ESC>",
   { desc = 'JSON.stringify variable with lines (above)' }
 )
