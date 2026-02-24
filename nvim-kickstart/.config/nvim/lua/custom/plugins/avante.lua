fdda6cb12c9c9397059985852a3f88e71d5899a4
Viktor Nagy <viktor.nagy1995@gmail.com>
2026-02-24 14:10:52 +0100

    fix(nvim): disable avante for the time being

--- nvim-kickstart/.config/nvim/lua/custom/plugins/avante.lua
+++ nvim-kickstart/.config/nvim/lua/custom/plugins/avante.lua
@@ -2,6 +2,7 @@ return {
   'yetone/avante.nvim',
   event = 'VeryLazy',
   lazy = false,
+  enabled = false,
   version = false, -- set this if you want to always pull the latest change
   opts = {
     hints = { enabled = false },
